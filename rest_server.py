9dec14eb1ac83593d80e6c1817459ff8b082c077
Tim R <tim.rothenpieler.extern@bdr.de>
2024-03-06 18:08:33 +0100

    add server bootstrap

--- rest_server.py
+++ rest_server.py
@@ -1,12 +1,12 @@
-from flask import Flask
+import uvicorn
+from fastapi import FastAPI
 
-app = Flask(__name__)
+app = FastAPI()
 
-
-@app.route('/')
+@app.get("/")
 def hello():
     return 'Hello, World!'
 
 
 if __name__ == '__main__':
-    app.run()
+    uvicorn.run('rest_server:app', host='0.0.0.0', port=8000)
